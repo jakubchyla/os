feb7397af6350d9cf419ee48d3bf6a504b86d1b5
Jakub Chyła <jakchyla@gmail.com>
2021-08-02 01:26:04 +0200

    added clear command to scripts, refactored

--- build.sh
+++ build.sh
@@ -3,13 +3,64 @@
 export KERNEL_SRC="$(dirname $(realpath -s $0))/kernel"
 export KERNEL_BUILD_DIR="$KERNEL_SRC/build"
 
-build_bootloader(){
-    if [ -z $EDK_PATH ]; then
-        EDK_PATH="$HOME/src/build/edk2"
-    fi
-    EDK_PATH=$EDK_PATH $KERNEL_SRC/boot/build-efi.sh
+build_bootloader()
+{
+    EDK_PATH=$EDK_PATH "$KERNEL_SRC/boot/build-efi.sh" build
 }
 
-mkdir $KERNEL_BUILD_DIR
+clear_build()
+{
+    "$KERNEL_SRC/boot/build-efi.sh" clear
+    rm -rf $KERNEL_BUILD_DIR
+}
 
-build_bootloader
+
+main()
+{
+    if [ ! -d "kernel" ]; then
+        printf "run this script from repo's root directory\n" >&2
+        exit 1
+    fi
+    if [ $# -eq 0 ]; then
+        printf "no option specified\n" >&2
+        exit 1
+    fi
+    
+    COMMAND=$1
+    shift
+
+    while [ $# -gt 0 ]; do
+        key=$1
+        case $key in
+            --edk)
+                EDK_PATH=$2
+                shift; shift
+            ;;
+            *)
+                printf "unknown option\n" >&2
+                exit 1
+            ;;
+        esac
+    done
+
+    case $COMMAND in
+        build-all|build)
+            mkdir $KERNEL_BUILD_DIR 2> /dev/null
+            build_bootloader
+        ;;
+        build-bootloader)
+            mkdir $KERNEL_BUILD_DIR 2> /dev/null
+            build_bootloader
+        ;;
+        clear)
+            clear_build
+        ;;
+        *)
+            printf "unknown command\n" >&2
+            exit 1
+        ;;
+    esac
+}
+
+
+main $@
--- kernel/boot/build-efi.sh
+++ kernel/boot/build-efi.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-generate_target(){
+generate_target()
+{
     printf "%s\n" \
         "ACTIVE_PLATFORM = bootloader/bootloader.dsc" \
         "TARGET          = $TARGET" \
@@ -10,28 +11,66 @@ generate_target(){
         "BUILD_RULE_CONF = Conf/build_rule.txt"
 }
 
-if [ -z "$EDK_PATH" ]; then
-    printf "EDK_PATH not set\n" >&2
-    exit -1
-fi
-if [ -z $TARGET ]; then
-    TARGET="DEBUG"
-fi
+build_efi()
+{
+    if [ -z "$EDK_PATH" ]; then
+        EDK_PATH="$HOME/src/build/edk2"
+    fi
+    if [ ! -d "$EDK_PATH" ]; then
+        printf "$EDK_PATH does not exist\n" >&2
+        exit 1
+    fi
+    if [ -z $TARGET ]; then
+        TARGET="DEBUG"
+    fi
 
-export WORKSPACE="$KERNEL_SRC/boot"
-export EDK_TOOLS_PATH="$EDK_PATH/BaseTools"
-export PACKAGES_PATH="$EDK_PATH"
+    export EDK_TOOLS_PATH="$EDK_PATH/BaseTools"
+    export PACKAGES_PATH="$EDK_PATH"
 
-mkdir "$WORKSPACE/Conf"
+    mkdir -p "$CONF_PATH"
 
-pushd "$EDK_PATH" > /dev/null
-source edksetup.sh
-popd > /dev/null
+    pushd "$EDK_PATH" > /dev/null
+    source edksetup.sh
+    popd > /dev/null
 
-generate_target > "$WORKSPACE/Conf/target.txt"
+    generate_target > "$WORKSPACE/Conf/target.txt"
 
-mkdir -p "$KERNEL_BUILD_DIR/boot"
+    mkdir -p "$KERNEL_BUILD_DIR/boot"
 
-pushd "$EDK_PATH" > /dev/null
-build -n $(nproc) -D "BUILD_DIR=$KERNEL_BUILD_DIR"
+    pushd "$EDK_PATH" > /dev/null
+    build -n $(nproc) -D "BUILD_DIR=$KERNEL_BUILD_DIR"
+}
 
+clear_build()
+{
+    rm -rf "$KERNEL_BUILD_DIR/boot"
+    rm -rf "$CONF_PATH"
+}
+
+main(){
+    export WORKSPACE="$KERNEL_SRC/boot"
+    export CONF_PATH="$WORKSPACE/Conf"
+
+    if [ $# -eq 0 ]; then
+        printf "no option given for build-efi.sh, leaving...\n" >&2
+        exit 1
+    fi
+
+    case $1 in
+        build)
+            build_efi
+            shift
+        ;;
+        clear)
+            clear_build
+            shift
+        ;;
+        *)
+            printf "unknown command\n" >&2
+            exit 1
+        ;;
+    esac
+}
+
+
+main $@
